38af330c1645b36043ad9d2e6e2975f68e0e86ec
Danny Davenport <ddavenport420@gmail.com>
2019-06-14 10:20:26 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
+import pdfMake from 'pdfmake/build/pdfmake.min';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
 import './App.css';
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+const pdfTemplate = {
+	content: [
+		'First paragraph',
+		'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+	]
+	
+};
+const generate = () => pdfMake.createPdf(pdfTemplate).download('file')
 
 function App() {
   return (
@@ -10,6 +22,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={generate}>download pdf</button>
         <a
           className="App-link"
           href="https://reactjs.org"
