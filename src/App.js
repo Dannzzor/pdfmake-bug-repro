a5b5e80f0857d08a66d015197c23858ba295a4db
Danny Davenport <ddavenport420@gmail.com>
2019-06-14 10:32:49 -0400

    add simple server for build folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import pdfMake from 'pdfmake/build/pdfmake.min';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
-import './App.css';
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 const pdfTemplate = {
 	content: [
-		'First paragraph',
-		'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+		'test',
 	]
 	
 };
@@ -17,21 +14,7 @@ const generate = () => pdfMake.createPdf(pdfTemplate).download('file')
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <button onClick={generate}>download pdf</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
